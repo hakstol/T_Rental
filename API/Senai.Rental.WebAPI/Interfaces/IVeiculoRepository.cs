b6295be51e7c5ce7d03de4ec5b1b509773719f62
Leonardo Vitor Ferreira da SIlva <61870020+lyogaki@users.noreply.github.com>
2021-09-27 14:15:18 -0300

    Create IVeiculoRepository.cs

---
+++ API/Senai.Rental.WebAPI/Interfaces/IVeiculoRepository.cs
@@ -0,0 +1,17 @@
+using Senai.Rental.WebAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebAPI.Interfaces
+{
+    interface IVeiculoRepository
+    {
+        List<VeiculoDomain> ListarTodos();
+        VeiculoDomain BuscarPorId(int idVeiculo);
+        void Cadastrar(VeiculoDomain novoVeiculo);
+        void AtualizarIdCorpo(VeiculoDomain veiculoAtualizado);
+        void Deletar(int idVeiculo);
+    }
+}
