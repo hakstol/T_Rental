8c96a25f3ca918d97f0ff521fb3cce7dcf6dd8de
Leonardo Vitor Ferreira da SIlva <61870020+lyogaki@users.noreply.github.com>
2021-09-27 14:16:40 -0300

    Create ModeloDomain.cs

---
+++ API/Senai.Rental.WebAPI/Domains/ModeloDomain.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebAPI.Domains
+{
+    public class ModeloDomain
+    {
+        public int idModelo { get; set; }
+        public int idMarca { get; set; }
+        public string nomeModelo { get; set; }
+        public MarcaDomain marca { get; set; }
+    }
+}
