874a2c138b3a8b2b9d0e217b545fa6c8e85794b6
Leonardo Vitor Ferreira da SIlva <61870020+lyogaki@users.noreply.github.com>
2021-09-27 14:16:53 -0300

    Create VeiculoDomain.cs

---
+++ API/Senai.Rental.WebAPI/Domains/VeiculoDomain.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebAPI.Domains
+{
+    public class VeiculoDomain
+    {
+        public int idVeiculo { get; set; }
+        public int idEmpresa { get; set; }
+        public int idModelo { get; set; }
+        public string placa { get; set; }
+        public EmpresaDomain empresa { get; set; }
+        public ModeloDomain modelo { get; set; }
+    }
+}
