8f8ccbb0214098fdde0fdcd0ad4ec8859d4717d0
Leonardo Vitor Ferreira da SIlva <61870020+lyogaki@users.noreply.github.com>
2021-09-27 14:16:15 -0300

    Create EmpresaDomain.cs

---
+++ API/Senai.Rental.WebAPI/Domains/EmpresaDomain.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebAPI.Domains
+{
+    public class EmpresaDomain
+    {
+        public int idEmpresa { get; set; }
+        public string nomeEmpresa { get; set; }
+    }
+}
